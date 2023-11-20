ca331a134f6b9e1d85dfe62e6cae8a596eba3399
Mohannad Oraby <mohannad.oraby@chargebyte.com>
2023-11-20 16:40:22 +0100

    CbTarragonDriver: Use CMake project settings
    - Created a 'configuration.h' to use it in the implementation.
    - Created a 'VERSION' file for the HW driver to pack it in the image.
    
    Signed-off-by: Mohannad Oraby <mohannad.oraby@chargebyte.com>

--- modules/CbTarragonDriver/CbTarragonDriver.cpp
+++ modules/CbTarragonDriver/CbTarragonDriver.cpp
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Pionix GmbH and Contributors to EVerest
 #include "CbTarragonDriver.hpp"
+#include "configuration.h"
 
 namespace module {
 
 void CbTarragonDriver::init() {
     invoke_init(*p_evse_board_support);
+
+    EVLOG_info << PROJECT_DESCRIPTION << " (version: " << PROJECT_VERSION << ")";
 }
 
 void CbTarragonDriver::ready() {
