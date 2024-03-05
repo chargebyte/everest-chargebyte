ac506bc32491414e43921fc53394ddc6e500f9b7
Mohannad Oraby <mohannad.oraby@chargebyte.com>
2024-03-05 08:49:57 +0000

    CbTarragonDriver: Adjust the maximum offered current
    This is checked against max_current_import_A parameter from EvseManager, and
    the minimum value of both is considered as the current offered. In the future,
    this should be determined on our side using the rotary encoder on the board.
    
    Signed-off-by: Mohannad Oraby <mohannad.oraby@chargebyte.com>

--- modules/CbTarragonDriver/evse_board_support/evse_board_supportImpl.cpp
+++ modules/CbTarragonDriver/evse_board_support/evse_board_supportImpl.cpp
@@ -34,11 +34,11 @@ void evse_board_supportImpl::init() {
     this->termination_requested = false;
 
     // Configure hardware capabilities
-    this->hw_capabilities.max_current_A_import = 16;
+    this->hw_capabilities.max_current_A_import = 32;
     this->hw_capabilities.min_current_A_import = 0;
     this->hw_capabilities.max_phase_count_import = 3;
     this->hw_capabilities.min_phase_count_import = 1;
-    this->hw_capabilities.max_current_A_export = 16;
+    this->hw_capabilities.max_current_A_export = 32;
     this->hw_capabilities.min_current_A_export = 0;
     this->hw_capabilities.max_phase_count_export = 3;
     this->hw_capabilities.min_phase_count_export = 1;
