a0fc0f3b8f02e4b0844f2b5f5b234be7ad2e372d
Mohannad Oraby <mohannad.oraby@chargebyte.com>
2024-06-03 06:30:53 +0000

    CbTarragonDriver: Add license information to souces files (refs EVEREST-831)
    Signed-off-by: Mohannad Oraby <mohannad.oraby@chargebyte.com>

--- modules/CbTarragonDriver/tarragon/CbTarragonContactorControl.hpp
+++ modules/CbTarragonDriver/tarragon/CbTarragonContactorControl.hpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright chargebyte GmbH and Contributors to EVerest
 #pragma once
 #include <chrono>
 #include <optional>
--- modules/CbTarragonDriver/tarragon/CbTarragonRCM.cpp
+++ modules/CbTarragonDriver/tarragon/CbTarragonRCM.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright chargebyte GmbH and Contributors to EVerest
 #include "CbTarragonRCM.hpp"
 #include <gpiod.hpp>
 #include <gpiodUtils.hpp>
