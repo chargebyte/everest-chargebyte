be8603d2cf8e5db7b69a50a20aa1ff7f6e5b944b
Michael Heimpold <michael.heimpold@chargebyte.com>
2025-11-04 15:40:15 +0100

    CbTarragonContactorControlSerial: prepare class for inheritance
    - relex the protection class for members
    - split switch_state into two sub-functions
    
    This allows later to derive classes from this one and re-use code as much as possible.
    
    Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

--- modules/CbTarragonDriver/tarragon/CbTarragonContactorControlSerial.hpp
+++ modules/CbTarragonDriver/tarragon/CbTarragonContactorControlSerial.hpp
@@ -44,12 +44,18 @@ public:
     /// @return A reference to the output stream operated on.
     friend std::ostream& operator<<(std::ostream& os, const CbTarragonContactorControlSerial& c);
 
-private:
+protected:
     /// @brief The primary contactor and its feedback abstraction.
     CbTarragonContactor primary;
 
     /// @brief The secondary contactor and its feedback abstraction.
     CbTarragonContactor secondary;
+
+    /// @brief Split `switch_state` into two dedicated functions so that we can override in derived classes.
+    virtual bool switch_state_on();
+
+    /// @brief Split `switch_state` into two dedicated functions so that we can override in derived classes.
+    virtual bool switch_state_off();
 
     /// @brief Helper to feed a string representation into an output stream.
     /// @param os Output stream reference to operate on.
