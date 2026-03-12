274360b1d1fa3b1d4d47d242c0afe756b2b1d91f
Michael Heimpold <michael.heimpold@chargebyte.com>
2026-03-12 12:37:28 +0100

    CbGPIOEnergyLimits: initial code generation based on manifest
    Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

---
+++ modules/CbGPIOEnergyLimits/CbGPIOEnergyLimits.cpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright chargebyte GmbH, Pionix GmbH and Contributors to EVerest
+#include "CbGPIOEnergyLimits.hpp"
+
+namespace module {
+
+void CbGPIOEnergyLimits::init() {
+}
+
+void CbGPIOEnergyLimits::ready() {
+}
+
+} // namespace module
